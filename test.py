aac724bee366d3213be903dea720b38403c62460
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2023-07-08 18:21:27 +1000

    if anyone has a good config that looks like real clouds please create a pull request

--- test.py
+++ test.py
@@ -11,82 +11,80 @@ from perlin_noise import PerlinNoise
 config = {
     "noise1": {
         "octaves": 3,
-        "base": 8,
+        "base": 16,
         "inner": 1
     },"noise2": {
         "octaves": 6,
-        "base": 4,
+        "base": 16,
         "inner": 1
     },"noise3": {
-        "octaves": 12,
+        "octaves": 16,
         "base": 2,
         "inner": 2
     },"noise4": {
-        "octaves": 24,
+        "octaves": 16,
         "base": 1,
         "inner": 4
     },"noise5": {
-        "octaves": 48,
+        "octaves": 16,
         "base": 1,
         "inner": 5
     }
 }
 
-seed = random.randint(1,1000)
+noise1 = PerlinNoise(octaves=config["noise1"]["octaves"], seed=random.randint(1,1000))
+noise2 = PerlinNoise(octaves=config["noise2"]["octaves"], seed=random.randint(1,1000))
+noise3 = PerlinNoise(octaves=config["noise3"]["octaves"], seed=random.randint(1,1000))
+noise4 = PerlinNoise(octaves=config["noise4"]["octaves"], seed=random.randint(1,1000))
+noise5 = PerlinNoise(octaves=config["noise5"]["octaves"], seed=random.randint(1,1000))
 
-noise1 = PerlinNoise(octaves=config["noise1"]["octaves"], seed=seed)
-noise2 = PerlinNoise(octaves=config["noise2"]["octaves"], seed=seed)
-noise3 = PerlinNoise(octaves=config["noise3"]["octaves"], seed=seed)
-noise4 = PerlinNoise(octaves=config["noise4"]["octaves"], seed=seed)
-noise5 = PerlinNoise(octaves=config["noise5"]["octaves"], seed=seed)
-
-xpix, ypix = 512, 512
+x, y = 512, 512
 pic = []
-for i in range(xpix):
+for i in range(x):
     row = []
-    for j in range(ypix):
+    for j in range(y):
         noise_val = config["noise1"]["base"]*noise1(
             [i/(
-                config["noise1"]["inner"]*xpix
+                config["noise1"]["inner"]*x
                 ),
             j/(
-                config["noise1"]["inner"]*ypix
+                config["noise1"]["inner"]*y
                 )
             ]
         )
         noise_val += config["noise2"]["base"]*noise1(
             [i/(
-                config["noise2"]["inner"]*xpix
+                config["noise2"]["inner"]*x
                 ),
             j/(
-                config["noise2"]["inner"]*ypix
+                config["noise2"]["inner"]*y
                 )
             ]
         )
         noise_val += config["noise3"]["base"]*noise1(
             [i/(
-                config["noise3"]["inner"]*xpix
+                config["noise3"]["inner"]*x
                 ),
             j/(
-                config["noise3"]["inner"]*ypix
+                config["noise3"]["inner"]*y
                 )
             ]
         )
         noise_val += config["noise4"]["base"]*noise1(
             [i/(
-                config["noise4"]["inner"]*xpix
+                config["noise4"]["inner"]*x
                 ),
             j/(
-                config["noise4"]["inner"]*ypix
+                config["noise4"]["inner"]*y
                 )
             ]
         )
         noise_val += config["noise5"]["base"]*noise1(
             [i/(
-                config["noise5"]["inner"]*xpix
+                config["noise5"]["inner"]*x
                 ),
             j/(
-                config["noise5"]["inner"]*ypix
+                config["noise5"]["inner"]*y
                 )
             ]
         )
